eb625c45ccb3ccf4b9e7c9ed8c65932f0af3d48b
n3rdn1nja <zackcpetersen@gmail.com>
2019-03-05 17:22:40 -0700

    fixed css and added this.props.business.zipCode

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -11,10 +11,10 @@ class Business extends React.Component {
   </div>
   <h2>{this.props.business.name}</h2>
   <div className="Business-information">
-    <div className="Business Address">
+    <div className="Business-address">
       <p>{this.props.business.address}</p>
       <p>{this.props.business.city}</p>
-      <p>{this.props.business.state}, {this.props.zipCode}</p>
+      <p>{this.props.business.state}, {this.props.business.zipCode}</p>
     </div>
     <div className="Business-reviews">
       <h3>{this.props.business.category}</h3>
